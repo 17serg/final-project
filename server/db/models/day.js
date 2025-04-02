2c31093d5c9ece6a9a34a668d7f9e7f5cd6f288d
17serg <bolonkin.s@outlook.com>
2025-04-02 19:13:09 +0300

    create model - day

---
+++ server/db/models/day.js
@@ -0,0 +1,38 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Day extends Model {
+    static associate({ User }) {
+      this.belongsTo(User, { foreignKey: 'userId' });
+    }
+  }
+  Day.init(
+    {
+      date: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          is: /^\d{2}\.\d{2}\.\d{4}$/, // формат дд.мм.гггг
+        },
+      },
+      isTraining: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Day',
+    },
+  );
+  return Day;
+};
